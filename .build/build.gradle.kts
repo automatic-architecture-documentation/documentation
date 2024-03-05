5fe6d803c3fc679ce8bfc2b119b152196559bff0
Stefan Ludwig <github@slu-it.de>
2024-03-05 18:51:24 +0100

    rename Gradle task to fit conventions

--- .build/build.gradle.kts
+++ .build/build.gradle.kts
@@ -2,7 +2,7 @@ import documentation.generateComponentDiagramsFromJson
 import documentation.generateDiagramsFromPlantUml
 import documentation.generateOverviewDiagramsFromJson
 
-tasks.register("generate-files") {
+tasks.register("generateFiles") {
     val srcFolder = File(project.rootDir, "src")
     val rootFolder = project.rootDir.parentFile
     doLast {
