1c779e935b3177ea906659c213ab406a8b9ddfac
Stefan Ludwig <github@slu-it.de>
2024-03-05 20:09:31 +0100

    reduce dependencies for jackson

--- .build/buildSrc/build.gradle.kts
+++ .build/buildSrc/build.gradle.kts
@@ -7,9 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:24.1.0")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.16.1")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.16.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
     implementation("net.sourceforge.plantuml:plantuml:1.2024.3")
