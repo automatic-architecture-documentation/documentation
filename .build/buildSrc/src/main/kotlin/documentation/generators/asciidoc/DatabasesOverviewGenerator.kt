f25400098ab48d23f8ec85b2bdf1859cb33f81bc
Stefan Ludwig <github@slu-it.de>
2024-09-03 21:43:03 +0200

    implement database document

--- .build/buildSrc/src/main/kotlin/documentation/generators/asciidoc/DatabasesOverviewGenerator.kt
+++ .build/buildSrc/src/main/kotlin/documentation/generators/asciidoc/DatabasesOverviewGenerator.kt
@@ -1,25 +1,66 @@
 package documentation.generators.asciidoc
 
 import documentation.model.Application
+import documentation.model.Database
+import documentation.model.Database.Table
 import documentation.model.Distance.OWNED
-import documentation.model.componentName
 
 class DatabasesOverviewGenerator(applications: List<Application>) {
 
-    private val ourApplicationsWithDatabases = applications
+    private val keyEmoji = "\uD83D\uDD11"
+    private val ourDatabases = applications
         .filter { it.distanceFromUs == OWNED }
-        .filter { it.databases.isNotEmpty() }
-        .sortedBy { componentName(it.id) }
+        .flatMap(Application::databases)
+        .sortedBy(Database::name)
 
     fun asciiDocSource(): String =
         buildString {
             appendLine(":toc: left")
-            appendLine(":toclevels: 2")
+            appendLine(":toclevels: 3")
             appendLine()
             appendLine("= Databases")
             appendLine()
-            ourApplicationsWithDatabases
-                .forEach { application ->
+            ourDatabases
+                .sortedBy(Database::name)
+                .forEach { database ->
+                    appendLine("== ${database.name}")
+                    appendLine()
+                    if (database.description != null) {
+                        appendLine(database.description)
+                        appendLine()
+                    }
+                    appendLine("Type: ${database.type}")
+                    appendLine()
+                    database.tables
+                        .sortedBy(Table::name)
+                        .forEach { table ->
+                            appendLine("=== Table: ${table.name}")
+                            appendLine()
+                            if (table.description != null) {
+                                appendLine(table.description)
+                                appendLine()
+                            }
+                            appendLine(".${table.name} columns")
+                            appendLine("[width=100%, cols=\"~,~,~,~,~\"]")
+                            appendLine("|===")
+                            appendLine("|Column |Type |Nullable? |Default Value |Description")
+                            appendLine()
+                            table.columns
+                                .forEach { column ->
+                                    if (column.partOfPrimaryKey) {
+                                        appendLine("|`$keyEmoji ${column.name}`")
+                                    } else {
+                                        appendLine("|`${column.name}`")
+                                    }
+                                    appendLine("|${column.dataType}")
+                                    appendLine("|${column.nullable}")
+                                    appendLine("|${column.defaultValue ?: ""}")
+                                    appendLine("|${column.description ?: ""}")
+                                    appendLine()
+                                }
+                            appendLine("|===")
+                            appendLine()
+                        }
                 }
         }
 }
